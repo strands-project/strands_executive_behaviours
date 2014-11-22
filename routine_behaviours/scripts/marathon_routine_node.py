c61ed17606045402f812a18d5e4b7b9b596b34ba
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-11-22 14:33:12 +0000

    Added more flexible options for patrol behaviours.

--- routine_behaviours/scripts/marathon_routine_node.py
+++ routine_behaviours/scripts/marathon_routine_node.py
@@ -2,7 +2,7 @@
 
 import rospy
 
-from datetime import time, timedelta
+from datetime import time, timedelta, datetime
 from dateutil.tz import tzutc, tzlocal
 
 from routine_behaviours.marathon_routine import MarathonRoutine
@@ -14,11 +14,16 @@ if __name__ == '__main__':
     # start and end times -- all times should be in a particular timezone - local has stopped working!
     # localtz = tzlocal()
     localtz = tzutc()
-    start = time(8,00, tzinfo=localtz)
+
+    now = datetime.now(localtz).time().replace(tzinfo=localtz)
+    # useful for testing
+    # start = now
+
+    start = time(8,00, tzinfo=localtz)    
     end = time(00,00, tzinfo=localtz)
 
     # how long to stand idle before doing something
-    idle_duration=rospy.Duration(20)
+    idle_duration=rospy.Duration(5)
 
     # how long do you want it to take to do a tour. this must be greater than the time you think it will take!
     # the number argument is in seconds
@@ -27,8 +32,32 @@ if __name__ == '__main__':
     routine = MarathonRoutine(daily_start=start, daily_end=end, 
         idle_duration=idle_duration, tour_duration_estimate=tour_duration_estimate)    
 
+
+    # choose which nodes are visited at random on idle
+    # routine.random_nodes = ['WayPoint2', 'WayPoint3']
+    # or
+    routine.random_nodes = routine.all_waypoints_except(['WayPoint2', 'WayPoint3'])
+
     # go around every node every tour_duration_estimate
-    routine.create_patrol_routine()
+    # routine.create_patrol_routine()
+
+    ten_am = time(10,00, tzinfo=localtz)
+    two_pm = time(14,00, tzinfo=localtz)
+
+    thirty_mins = timedelta(minutes = 30)
+    sixty_mins = timedelta(minutes = 60)
+    ninety_mins = timedelta(minutes = 90)
+
+
+    # patrol just these selected waypoints every 30 minutes in the first part of the day
+    routine.create_patrol_routine(waypoints=['WayPoint2', 'WayPoint3'], daily_start=start, daily_end=ten_am, repeat_delta=thirty_mins)
+    
+    # then all but these every 60 minutes in the second part part of the day
+    routine.create_patrol_routine(waypoints=routine.all_waypoints_except(['WayPoint2', 'WayPoint3']), daily_start=ten_am, daily_end=two_pm, repeat_delta=sixty_mins)
+    
+    # then all waypoints these every 90 minutes in the final part of the day
+    routine.create_patrol_routine(waypoints=routine.all_waypoints(), daily_start=two_pm, daily_end=end, repeat_delta=ninety_mins)
+ 
 
     # do 3d scans
     scan_waypoints = ['WayPoint2', 'WayPoint3']
