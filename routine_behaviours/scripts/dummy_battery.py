c45b9ba129e69b901d62cd8919beabd7c0a6cce5
Nick Hawes <nickh@robots.ox.ac.uk>
2018-01-31 17:48:41 +0000

    Updated battery script to use sensor_msgs/BatteryState

--- routine_behaviours/scripts/dummy_battery.py
+++ routine_behaviours/scripts/dummy_battery.py
@@ -2,7 +2,7 @@
 
 import rospy
 
-from scitos_msgs.msg import BatteryState
+from sensor_msgs.msg import BatteryState
 from std_msgs.msg import String
 
 class DummyBattery(object):
@@ -11,13 +11,17 @@ class DummyBattery(object):
     """
     def __init__(self):
         super(DummyBattery, self).__init__()
-        self._charging_points = rospy.get_param('~charging_points', ['ChargingPoint'])
+        self._charging_points = rospy.get_param('~charging_points', ['ChargingPoint', 'ChargingPoint1', 'ChargingPoint2' ])
         self._current_node = None
-        # run at 10hz which matches the scitos
+        
+        # run at 10hz which matches the scitos robot
         self._rate = 10
+
         # battery percent per second
         self._discharge_rate = float(rospy.get_param('~discharge_rate', 0.03)) / self._rate
         self._recharge_rate = float(rospy.get_param('~recharge_rate', 1.0)) / self._rate
+
+        # battery charge between 1 and 100
         self._current_level = 100
 
         rospy.Subscriber('/current_node', String, self._update_topological_location)
@@ -32,16 +36,17 @@ class DummyBattery(object):
         rate = rospy.Rate(self._rate)
 
         msg = BatteryState()
+
         while not rospy.is_shutdown():
             msg.header.stamp = rospy.get_rostime()
-            msg.charging = self._current_node in self._charging_points
+            msg.present = self._current_node in self._charging_points
 
-            if msg.charging: 
+            if msg.present: 
                 self._current_level = min(100, self._current_level + self._recharge_rate)
             else:
                 self._current_level = max(0, self._current_level - self._discharge_rate)
 
-            msg.lifePercent = int(self._current_level)
+            msg.percentage = float(self._current_level)
             self._battery_pub.publish(msg)            
             rate.sleep()
 
